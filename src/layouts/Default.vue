ed736f2d3bf5150c4d7486e1a62aaa69e45c9d0d
Rick Berger <gbergeraph@gmail.com>
2020-04-25 11:46:55 -0600

    Added border under header

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto px-4 pt-10 h-screen text-gray-600">
-    <header class="flex justify-between items-center">
+    <header class="flex justify-between items-center border-b-2 border-green-600">
       <div class="font-display text-green-700 font-hairline text-6xl tracking-wider">Rickb Recipes</div>
       <nav>
         <g-link class="px-4 text-xl hover:text-blue-600" to="/">Home</g-link>
