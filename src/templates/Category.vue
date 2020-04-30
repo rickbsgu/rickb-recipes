5c6c57cf7c5dcf3b50e658e250ef786517570e48
Rick Berger <gbergeraph@gmail.com>
2020-04-29 18:51:07 -0600

    Mocked up the category page.

--- src/templates/Category.vue
+++ src/templates/Category.vue
@@ -1,11 +1,29 @@
 <template>
   <Layout>
-    <div text-4xl pt-10>Recipes from: <span class="capitalize">{{ $context.categoryName }}</span> </div>
-    <li v-for="(recipe, ix) in recipesForCategory" :key="ix">
-      <g-link :to="recipe.path">
-        {{recipe.title}}
-      </g-link>
-    </li>
+    <div>
+      <div class="font-thin italic text-6xl pt-2 text-center text-green-600 capitalize">{{ $context.categoryName }}
+      <sub class="font-normal text-gray-600 text-3xl -ml-4">Recipes</sub></div>
+    </div>
+    <div class="px-8">
+    <div class="border-4 bg-yellow-100 border-gray-500 py-4 px-8 rounded-xl my-8">
+      <p>I used to be a devotee of package sauces.  They were easy &ndash; just peel open a package, dump it into a pan, add some water or milk, heat and stir.</p>
+      <p>This was before I learned how to make real hollandaise, with real egg yolks and lemon juice.</p>
+      <p>It was a transformative experience.  Looking at the package ingredients, there was no eggs or lemon juice to be found.  Same thing with the alfredo packets we used to crave &ndash; no cream, no parmesan, none of that.</p>
+    </div>
+    <div
+      class="flex border border-gray-500 border-2 rounded-r-lg"
+      v-for="(recipe, ix) in recipesForCategory" :key="ix">
+      <img style="height:120px;width:120px" src="/images/Sausage and Peppers.png"/>
+      <div class="pl-4 py-2 text-2xl">
+        <div> 
+          <g-link :to="recipe.path">
+          {{recipe.title}}
+          </g-link>
+        </div>
+      <p>One of the four basic sauces.  It's a bit tricky to master, but incredibly rewarding once you get it down.</p>
+      </div>
+    </div>
+    </div>
   </Layout>
 </template>
 
