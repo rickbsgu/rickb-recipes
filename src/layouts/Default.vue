1e89fa663da2a6bab2dce8e045b50a41bc716842
Rick Berger <gbergeraph@gmail.com>
2020-05-01 16:09:38 -0600

    Less padding at header - better on smaller devices

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="container mx-auto px-4 pt-10 h-screen text-gray-600">
+  <div class="container mx-auto px-4 pt-4 h-screen text-gray-600">
     <header class="flex justify-between items-center border-b-2 border-green-600">
-      <div class="font-display text-green-700 font-hairline text-6xl tracking-wider">Rickb Recipes</div>
+      <div class="font-display text-green-700 font-hairline text-6xl tracking-wider">
+        Rickb Recipes
+      </div>
       <nav>
         <g-link class="px-4 text-xl" to="/">Home</g-link>
         <g-link class="px-4 text-xl" to="/about/">About</g-link>
